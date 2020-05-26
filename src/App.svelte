1e25b37aa3711f560710753564eac637195bd478
Voogie <vugar.adbulzada@gmail.com>
2020-05-26 17:20:28 +0200

    Deuglifying

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-	import {clipIt} from "./helpers" 
+	import {clipIt, isValidUrl} from "./helpers" 
 
-	let urlInput; 
+	let urlInput = ""; 
+	$: validUrl = isValidUrl(urlInput);
+	$: console.log(" >>> " + urlInput +" >>> validity is: " + validUrl)
 	let shortUrl;
 	const baseUrl = 'https://go.slit.link/';
 
 	const handleSubmit = () => {
+		if (!urlInput) return;
 		const apiUrl = baseUrl + 'generate';
 		const data = {
 			url: urlInput
@@ -29,44 +32,77 @@
 </script>
 
 <main>
-	<h2>Entered: {urlInput || "none"}</h2>
+	<div class="omnio">slit.link</div>
+	<h2 class="omnio">
+		> <span class="fancy-font">{urlInput}</span>
+	</h2>
 	<form on:submit|preventDefault={handleSubmit}>
-		<input bind:value={urlInput}>
-		<button disabled={!urlInput} type=submit>
+		<input class="basic-input" bind:value={urlInput} disabled={!!shortUrl}>
+		<button disabled={!validUrl || !urlInput || !!shortUrl} type=submit>
 			Submit
 		</button>
 	</form>
 	{#if shortUrl}
 		<section>
-			<input id="shortened" readonly value={shortUrl}>
-			<div on:click={() => clipIt("shortened")}>copy</div>
+			<input id="shortened" class="fancy-font" readonly value={shortUrl}>
+			<div class="copy" on:click={() => clipIt("shortened")}>click to copy</div>
 		</section>
 	{/if}
 </main>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap');
+	
+	* {
+		font-family: Helvetica, sans-serif
+	}
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 2em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h2 {
+	.omnio {
 		color: #999999;
 		font-size: 2em;
 		font-weight: 100;
 	}
 
 	form {
-		margin-bottom: 1em;
+		margin-bottom: 6em;
+	}
+
+	.fancy-font {
+		font-family: Poppins, Helvetica, sans-serif;
+		font-weight: 100;
+	}
+
+	.basic-input {
+		width: 40%
+	}
+
+	.copy {
+		cursor: pointer;
+		transition: all 200ms cubic-bezier(0.215, 0.610, 0.355, 1)
+	}
+
+	.copy:hover {
+		font-weight: 600;
+	}
+
+	.copy:active {
+		transform: scale(0.8);
 	}
 
 	#shortened {
+		text-align: center;
 		width: 60%;
 		color: #333333;
-		font-size: 2.5em;
-		font-weight: 260;
+		font-size: 2em;
+		font-weight: 300;
+		border: none;
+		border-top: 1px solid #666;
 	}
 
 	#shortened:focus {
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,3 +7,15 @@ export const clipIt = (inputId) => {
   document.execCommand("copy");
   return node.value;
 } 
+
+export const isValidUrl = (input) => {
+  if (!input.startsWith("https://") && !input.startsWith("http://")) 
+    return false;
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(input);
+}
